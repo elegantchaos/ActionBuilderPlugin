bf525249ab6875642655f472855ef1000a8f84ba
Sam Deane <sam@elegantchaos.com>
2022-07-05 12:33:55 +0100

    Fixed default configuration.

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
     ],
     
     dependencies: [
-        .package(url: "https://github.com/elegantchaos/ActionBuilderCore", from: "1.0.2")
+        .package(url: "https://github.com/elegantchaos/ActionBuilderCore", from: "1.0.3")
     ],
     
     targets: [
