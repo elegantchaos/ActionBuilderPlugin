d6216a308a854f60c7ca3f342ea09bfbb97fa293
Sam Deane <sam@elegantchaos.com>
2025-08-28 14:54:31 +0100

    updated formatting

--- Package.swift
+++ Package.swift
@@ -8,42 +8,42 @@
 import PackageDescription
 
 let package = Package(
-    name: "ActionBuilderPlugin",
-    platforms: [
-        .macOS(.v12)
-    ],
-    
-    products: [
-        .plugin(
-            name: "ActionBuilderPlugin",
-            targets: [
-                "ActionBuilderPlugin"
-            ]
+  name: "ActionBuilderPlugin",
+  platforms: [
+    .macOS(.v12)
+  ],
+
+  products: [
+    .plugin(
+      name: "ActionBuilderPlugin",
+      targets: [
+        "ActionBuilderPlugin"
+      ]
+    )
+  ],
+
+  dependencies: [
+    .package(url: "https://github.com/elegantchaos/ActionBuilderCore.git", from: "2.0.0")
+  ],
+
+  targets: [
+    .plugin(
+      name: "ActionBuilderPlugin",
+
+      capability: .command(
+        intent: .custom(
+          verb: "generate-workflow",
+          description: "Generates a Github Actions workflow file for the package."
         ),
-    ],
-    
-    dependencies: [
-        .package(url: "https://github.com/elegantchaos/ActionBuilderCore.git", from: "2.0.0")
-    ],
-    
-    targets: [
-        .plugin(
-            name: "ActionBuilderPlugin",
-            
-            capability: .command(
-                intent: .custom(
-                    verb: "generate-workflow",
-                    description: "Generates a Github Actions workflow file for the package."
-                ),
-                
-                permissions: [
-                    .writeToPackageDirectory(reason: "Generates/updates Workflow.yml and README.md.")
-                ]
-            ),
-            
-            dependencies: [
-                .product(name: "ActionBuilderTool", package: "ActionBuilderCore")
-            ]
-        ),
-    ]
+
+        permissions: [
+          .writeToPackageDirectory(reason: "Generates/updates Workflow.yml and README.md.")
+        ]
+      ),
+
+      dependencies: [
+        .product(name: "ActionBuilderTool", package: "ActionBuilderCore")
+      ]
+    )
+  ]
 )
--- Plugins/ActionBuilderPlugin/ActionBuilderPlugin.swift
+++ Plugins/ActionBuilderPlugin/ActionBuilderPlugin.swift
@@ -7,40 +7,40 @@ import Foundation
 import PackagePlugin
 
 @main struct ActionBuilderPlugin: CommandPlugin {
-    func run(tool: String, arguments: [String], context: PackagePlugin.PluginContext, cwd: URL) throws -> String {
-        let tool = try context.tool(named: tool)
-        
-        Diagnostics.remark("Running \(tool) \(arguments.joined(separator: " ")).")
-        
-        let process = Process()
-        process.executableURL = URL(fileURLWithPath: tool.path.string)
-        process.arguments = arguments
-        process.currentDirectoryURL = cwd
-        
-        let outputPipe = Pipe()
-        process.standardOutput = outputPipe
-        try process.run()
-        process.waitUntilExit()
-        
-        let data = outputPipe.fileHandleForReading.readDataToEndOfFile()
-        return String(decoding: data, as: UTF8.self)
-    }
-    
-    func performCommand(context: PackagePlugin.PluginContext, arguments: [String]) async throws {
-        let packageDirectoryURL = URL(fileURLWithPath: context.package.directory.string)
+  func run(tool: String, arguments: [String], context: PackagePlugin.PluginContext, cwd: URL) throws -> String {
+    let tool = try context.tool(named: tool)
 
-        // create workflows directory if necessary
-        let fm = FileManager.default
-        let workflowsURL = packageDirectoryURL.appendingPathComponent(".github/workflows")
-        if !fm.fileExists(atPath: workflowsURL.path) {
-            Diagnostics.remark("Creating workflows directory.")
-            try fm.createDirectory(at: workflowsURL, withIntermediateDirectories: true)
-        }
-        
-        // run the ActionBuilder command line tool
-        var toolArguments = [packageDirectoryURL.path]
-        toolArguments.append(contentsOf: arguments)
-        let output = try run(tool: "ActionBuilderTool", arguments: toolArguments, context: context, cwd: packageDirectoryURL)
-        Diagnostics.remark(output)
+    Diagnostics.remark("Running \(tool) \(arguments.joined(separator: " ")).")
+
+    let process = Process()
+    process.executableURL = URL(fileURLWithPath: tool.path.string)
+    process.arguments = arguments
+    process.currentDirectoryURL = cwd
+
+    let outputPipe = Pipe()
+    process.standardOutput = outputPipe
+    try process.run()
+    process.waitUntilExit()
+
+    let data = outputPipe.fileHandleForReading.readDataToEndOfFile()
+    return String(decoding: data, as: UTF8.self)
+  }
+
+  func performCommand(context: PackagePlugin.PluginContext, arguments: [String]) async throws {
+    let packageDirectoryURL = URL(fileURLWithPath: context.package.directory.string)
+
+    // create workflows directory if necessary
+    let fm = FileManager.default
+    let workflowsURL = packageDirectoryURL.appendingPathComponent(".github/workflows")
+    if !fm.fileExists(atPath: workflowsURL.path) {
+      Diagnostics.remark("Creating workflows directory.")
+      try fm.createDirectory(at: workflowsURL, withIntermediateDirectories: true)
     }
+
+    // run the ActionBuilder command line tool
+    var toolArguments = [packageDirectoryURL.path]
+    toolArguments.append(contentsOf: arguments)
+    let output = try run(tool: "ActionBuilderTool", arguments: toolArguments, context: context, cwd: packageDirectoryURL)
+    Diagnostics.remark(output)
+  }
 }
